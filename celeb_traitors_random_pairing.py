8f751c8dbac6c365c793da7db9a5fbe3dfbed0b8
Andrew Cutts <acutts@acgeospatial.co.uk>
2025-10-05 11:43:43 +0100

    Add files via upload

---
+++ celeb_traitors_random_pairing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from random import shuffle
+
+# read a list of the celebrity traitors into a dataframe
+traitors_df = pd.read_csv(r'C:\Data\Andrew\celeb_traitors\celeb_traitors.csv')
+
+# extract the traitor name as a list
+names = traitors_df['traitor'].tolist()
+# shuffle the names so order is random
+shuffle(names)
+
+# create a list of friends ['Andy', 'Catherine', 'Elodie'...] that matches length of traitors (19)
+friends = [i for i in range(19)] # place holder just numbers at present
+# shuffle the friends so order is random
+shuffle(friends)
+
+# create the pairings with zip
+pairings = zip(friends, names)
+# iterate over zip object as its a generator (could be converted to list)
+for pair in pairings:
+    print(pair)
